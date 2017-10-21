25305c9c4b8643b6b0286d8e50719090c90de3ec
Siddhant Rai <siddhantraisgr@gmail.com>
2017-10-21 20:08:15 +0530

    fic

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-var myPythonScriptPath = 'script.py';
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+//const token = process.env.FB_VERIFY_TOKEN
+//const access = process.env.FB_ACCESS_TOKEN
+const myPythonScriptPath = './test/sample.py';
 
 // Use python shell
-var PythonShell = require('python-shell');
-var pyshell = new PythonShell(myPythonScriptPath);
+const PythonShell = require('python-shell');
+const pyshell = new PythonShell(myPythonScriptPath);
 
-pyshell.on('message', function (message) {
-    // received a message sent from the Python script (a simple "print" statement)
-    console.log(message);
-});
 
-// end the input stream and allow the process to exit
-pyshell.end(function (err) {
-    if (err){
-        throw err;
-    };
+app.set('port', (process.env.PORT || 5000))
 
-    console.log('finished');
-});
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// ROUTES
+app.get('/', function(req, res) {
+    pyshell.on('message', function (message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        console.log(message);
+    });
+    
+    // end the input stream and allow the process to exit
+    pyshell.end(function (err) {
+        if (err){
+            throw err;
+        };
+    
+        console.log('finished');
+    });
+	res.send("Hi, I am Darwin your personal health assistant!")
+})
