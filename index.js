acc1e299d745fd6aaef9109d9dde7798332964a3
Siddhant Rai <siddhantraisgr@gmail.com>
2017-10-21 20:49:31 +0530

    multiple buildpacks fix web

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-var myPythonScriptPath = './test/sample.py';
+'use strict'
 
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+//const token = process.env.FB_VERIFY_TOKEN
+//const access = process.env.FB_ACCESS_TOKEN
+
+const myPythonScriptPath = './test/sample.py';
 // Use python shell
-var PythonShell = require('python-shell');
-var pyshell = new PythonShell(myPythonScriptPath);
+const PythonShell = require('python-shell');
 
-pyshell.on('message', function (message) {
-    // received a message sent from the Python script (a simple "print" statement)
-    console.log(message);
-});
+app.set('port', (process.env.PORT || 5000))
+// Allows us to process the data
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-// end the input stream and allow the process to exit
-pyshell.end(function (err) {
-    if (err){
-        throw err;
-    };
+// ROUTES
+app.get('/', function(req, res) {
+    const pyshell = new PythonShell(myPythonScriptPath);
+    pyshell.on('message', function (message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        console.log(message);
+    });  
+    // end the input stream and allow the process to exit
+    pyshell.end(function (err) {
+        if (err){
+            throw err;
+        };
+        console.log('finished');
+    });
+    res.send("Hi, I am Darwin your personal health assistant!")
+})
 
-    console.log('finished');
-});
+// run app
+app.listen(app.get('port'), function() {
+	console.log("running: "+app.get('port'))
+})
