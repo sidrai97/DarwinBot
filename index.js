d75f3b5a03406ebffa98ba576f3eeb4e7cad5694
Siddhant Rai <siddhantraisgr@gmail.com>
2017-10-22 06:28:47 +0530

    changed nlp fix

--- index.js
+++ index.js
@@ -43,8 +43,7 @@ app.post('/webhook', function (req, res) {
                 if (event.message) {
                     //receivedMessage(event)
                     console.log("Webhook received message event: ", event)
-                    nlpData=JSON.stringify(event.message.nlp)
-                    console.log(nlpData)
+                    console.log(JSON.stringify(event.message.nlp))
                 }
                 else if (event.postback){
                     //receivedPostback(event)
