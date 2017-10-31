0214f0cff000b4e94b1e3db5b2e056cd8646be24
Siddhant Rai <siddhantraisgr@gmail.com>
2017-10-31 14:44:06 +0530

    pymongo req, handle event in py

--- msgNlp/main.py
+++ msgNlp/main.py
@@ -11,11 +11,24 @@ senderId=eventObject['sender']['id']
 messageText="Can't understand your message"
 
 # 
-if 'text' in eventObject['message']:
-    messageText=eventObject['message']['text']
-    messageHandler.sendTextMessage(senderId,messageText)
-elif eventObject['postback']['payload'] == 'get_started'
-    messageText="Hi! I'm Darwin your personal Health Assistant. I can help you achieve your goal of 'Good Health'. To know more checkout the 'Features Menu' or simply say 'help'."
-    messageHandler.sendTextMessage(senderId,messageText)
+if 'postback' in eventObject:
+    if eventObject['postback']['payload'] == 'get_started':
+        messageText="Hi! I'm Darwin your personal Health Assistant. I can help you achieve your goal of 'Good Health'. To know more checkout the 'Features Menu' or simply say 'help'."
+        messageHandler.sendTextMessage(senderId,messageText)
+    elif eventObject['postback']['payload'] == 'symptom_checker':
+        pass
+    elif eventObject['postback']['payload'] == 'plan_my_workout':
+        pass
+    elif eventObject['postback']['payload'] == 'get_workout_recommendations':
+        pass
+    elif eventObject['postback']['payload'] == 'my_workout_statistics':
+        pass
+    elif eventObject['postback']['payload'] == 'exercises_guide':
+        pass
+elif 'message' in eventObject:
+    if 'text' in eventObject['message']:
+        #echo text msg
+        messageText=eventObject['message']['text']
+        messageHandler.sendTextMessage(senderId,messageText)
 else:
     messageHandler.sendTextMessage(senderId,messageText)
